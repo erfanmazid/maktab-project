ab6b543fe4893f99a7a860b37c869ec25014dec0
erfan mazidabadi <erfanmazidam@gmail.com>
2024-06-23 22:08:58 +0330

    creat and install all file and library need

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "./mail.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
