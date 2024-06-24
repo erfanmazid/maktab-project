4abc77eaa298c3b5592080c4532c62423dfdc9ff
erfan mazidabadi <erfanmazidam@gmail.com>
2024-06-24 11:13:41 +0330

    add router loading

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 import "./index.css";
 import Navigo from "navigo";
+import loadingPage from "./src/pages/loadingPage";
+
+export const router = new Navigo("/");
+export const routes = {
+  loading: "/",
+};
+
+router.on(routes.loading, loadingPage).resolve();
