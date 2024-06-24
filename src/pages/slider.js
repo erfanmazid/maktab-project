a4dd8930c718f68201436d02586c9852e7a4c0f9
erfan mazidabadi <erfanmazidam@gmail.com>
2024-06-24 21:26:06 +0330

    complit

--- src/pages/slider.js
+++ src/pages/slider.js
@@ -4,18 +4,27 @@ export function slider() {
   <div class="splide__track ">
 		<ul class="splide__list h-full">
 			<li class="splide__slide">
-                <img src="src/img/welcom/1.png" alt="">
+                <img src="src/img/welcom/1.png" class="-mt-5" alt="">
+                <div class="p-6 flex justify-center items-center">
+                  <p class="font-semibold text-[32px] text-center">We provide high quality products just for you</p>
+                </div>
             </li>
 			<li class="splide__slide">
-                <img src="src/img/welcom/2.png" alt="">
+                <img src="src/img/welcom/2.png" class="-mt-5" alt="">
+                <div class="p-6 flex justify-center items-center">
+                  <p class="font-semibold text-[32px] text-center">Your satisfaction is our number one periority</p>
+                </div>
             </li>
 			<li class="splide__slide">
                 <img src="src/img/welcom/3.png" alt="">
+                <div class="p-6 flex justify-center class="-mt-5" items-center">
+                  <p class="font-semibold text-[32px] text-center">Let’s fulfill your fashion needs with shoearight now!</p>
+                </div>
             </li>
 		</ul>
   </div>
-  <div class=" p-6 flex flex-col gap-8">
-  <button id="next-btn" class="text-[14px] w-full bg-[#212529] rounded-full h-[50px] flex justify-center items-center mt-12 text-white">Next</button>
+  <div class=" p-4 flex flex-col ">
+  <button id="next-btn" class="text-[14px] w-full bg-[#212529] rounded-full h-[50px] flex justify-center items-center text-white">Next</button>
   </div>
 </section>
     `;
