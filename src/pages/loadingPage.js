3e1ed3768e1a7b8c22be5624c4301c82ba6429c9
erfan mazidabadi <erfanmazidam@gmail.com>
2024-06-24 13:06:42 +0330

    add welcome

--- src/pages/loadingPage.js
+++ src/pages/loadingPage.js
@@ -25,5 +25,27 @@ export default function loadingPage() {
       </div>
 
   `;
-  setTimeout(router.navigate(routes.welcom), 3000);
+  setTimeout(welcomePage, 3000);
+}
+function welcomePage() {
+  document.querySelector("#app").innerHTML = `
+      <div class="relative h-screen">
+    <div>
+      <img src="./src/img/welcom/bg.png" class=" h-screen w-full object-cover " alt="" />
+    </div>
+    <div
+      class="bg-gradient-to-t from-black/80 to-transparent w-full h-full absolute top-0"
+    ></div>
+    <div class="absolute text-white bottom-[70px] ml-7">
+      <h2 class="font-semibold text-[40px]">Welcome to 👋</h2>
+      <h1 class="text-[72px] font-bold mb-4">Shoea</h1>
+      <p class="font-semibold text-[16px]">
+        The best sneakers & shoes e-commerse app of<br />
+        the century for your fashion needs!
+      </p>
+    </div>
+  </div>
+  
+      `;
+  setTimeout(welcomePage, 10000);
 }
