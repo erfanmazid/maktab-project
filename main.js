ba59251e6fcbe197cf17a7620178c832efc96bc6
erfan mazidabadi <erfanmazidam@gmail.com>
2024-06-24 19:22:08 +0330

    add signup page and splide

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 import "./index.css";
 import Navigo from "navigo";
 import loadingPage from "./src/pages/loadingPage";
-
+import "@splidejs/splide/css";
+import { signupPage } from "./src/pages/signupPage";
+export const root = document.querySelector("#app");
 export const router = new Navigo("/");
 export const routes = {
   loading: "/",
+  signup: "/signup",
 };
 
-router.on(routes.loading, loadingPage).resolve();
+router.on(routes.loading, loadingPage).on(routes.signup, signupPage).resolve();
