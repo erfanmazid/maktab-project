a4b2c36f3677b3e23365778427b2f27c83c2c9f4
erfan mazidabadi <erfanmazidam@gmail.com>
2024-06-25 22:32:34 +0330

    add home to router

--- main.js
+++ main.js
@@ -4,16 +4,20 @@ import loadingPage from "./src/pages/loadingPage";
 import "@splidejs/splide/css";
 import { signupPage } from "./src/pages/signupPage";
 import loginPage from "./src/pages/loginPage";
+import homePage from "./src/pages/home";
 export const root = document.querySelector("#app");
+
 export const router = new Navigo("/");
 export const routes = {
   loading: "/",
   signup: "/signup",
   login: "/login",
+  home: "/home",
 };
 
 router
   .on(routes.loading, loadingPage)
   .on(routes.signup, signupPage)
   .on(routes.login, loginPage)
+  .on(routes.home, homePage)
   .resolve();
