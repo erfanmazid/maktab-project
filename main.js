febc2fc4648225c869087a6c72cc3a22d3a70184
erfan mazidabadi <erfanmazidam@gmail.com>
2024-06-25 22:18:49 +0330

    add login page to router

--- main.js
+++ main.js
@@ -3,11 +3,17 @@ import Navigo from "navigo";
 import loadingPage from "./src/pages/loadingPage";
 import "@splidejs/splide/css";
 import { signupPage } from "./src/pages/signupPage";
+import loginPage from "./src/pages/loginPage";
 export const root = document.querySelector("#app");
 export const router = new Navigo("/");
 export const routes = {
   loading: "/",
   signup: "/signup",
+  login: "/login",
 };
 
-router.on(routes.loading, loadingPage).on(routes.signup, signupPage).resolve();
+router
+  .on(routes.loading, loadingPage)
+  .on(routes.signup, signupPage)
+  .on(routes.login, loginPage)
+  .resolve();
