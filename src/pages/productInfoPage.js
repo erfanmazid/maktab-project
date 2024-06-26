dded4dea58ba6d3fc86e58b7b9e74e13408ae5cd
erfan mazidabadi <erfanmazidam@gmail.com>
2024-06-26 16:05:37 +0330

    get id of product

--- src/pages/productInfoPage.js
+++ src/pages/productInfoPage.js
@@ -1,3 +1,5 @@
-export default function productInfoPage() {
+export default function productInfoPage(match) {
+  const id = match.data.id;
+  console.log(id);
   document.querySelector("#app").innerHTML = `info`;
 }
