635bf46080c42af43193ac72f648012201ae3e97
erfan mazidabadi <erfanmazidam@gmail.com>
2024-06-26 13:02:39 +0330

    change api adress

--- src/pages/api.js
+++ src/pages/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const baseUrl = "http://localhost:3000";
+export const baseUrl = "http://localhost:3001";
 
 axios.defaults.baseURL = baseUrl;
 export default axios;
