0650c6c8e4d961404e334e857b4894eb88acb8b2
erfan mazidabadi <erfanmazidam@gmail.com>
2024-06-27 07:16:59 +0330

    add search but have bug

--- src/pages/search.js
+++ src/pages/search.js
@@ -2,17 +2,20 @@ import axios from "./api";
 import { router, routes } from "../../main";
 
 export default async function searchPage(e) {
+  localStorage.getItem("searchHistory");
   const prod = await productData(e.params.q);
   if (prod.length == 0) {
-    renderHTML(e);
+    renderHTML(e, prod);
     let inp = document.querySelector("#searchInp");
     inp.value = e.params.q;
   } else {
-    document.querySelector("#app").innerHTML = `hi`;
+    renderHTMLFound(e, prod);
+    let inp = document.querySelector("#searchInp");
+    inp.value = e.params.q;
   }
 }
 
-function renderHTML(e) {
+function renderHTML(e, prod) {
   document.querySelector("#app").innerHTML = `
   <div class="flex flex-col justify-start items-center h-screen pt-4">
   <div class="w-[380px] mt-2 relative flex items-center">
@@ -35,7 +38,7 @@ function renderHTML(e) {
     <p class="text-[20px] font-bold">
       Result for <span>"${e.params.q}"</span>
     </p>
-    <p><span id="num"></span> found</p>
+    <p><span>${prod.length}</span> found</p>
   </div>
   <div class="flex flex-col items-center mt-24">
     <img
@@ -54,14 +57,68 @@ function renderHTML(e) {
     `;
 }
 
+function renderHTMLFound(e, prod) {
+  document.querySelector("#app").innerHTML = `
+    <div class="flex flex-col justify-start items-center h-screen pt-4">
+    <div class="w-[380px] mt-2 relative flex items-center">
+      <input
+        type="text"
+        id="searchInp"
+        class="bg-gray-100/85 w-full rounded-xl border border-black py-4 px-10 placeholder:text-[14px] placeholder:text-[#BAB8BC]"
+        placeholder="Search"
+        oninput="type()"
+      />
+      <i
+        class="fa-solid fa-magnifying-glass absolute left-[14px] text-[#6C757D] text-[18px]"
+      ></i>
+      <i
+        class="fa-solid fa-bars-progress absolute right-[14px]"
+        onclick="search()"
+      ></i>
+    </div>
+    <div class="flex justify-between pt-4 w-[380px] items-center">
+      <p class="text-[20px] font-bold">
+        Result for <span>"${e.params.q}"</span>
+      </p>
+      <p><span>${prod.length}</span> founds</p>
+    </div>
+    <div class="flex flex-col gap-y-3 w-full">
+    <div
+      class="flex flex-wrap gap-5 p-5 overflow-y-auto items-center no-scrollbar"
+    >
+    ${prod
+      .map((product) => {
+        return `
+    <a href="/product/${product.id}" data-navigo>
+      <div class="w-[182px] h-[244px] flex flex-col gap-y-1 basis-[47%]">
+        <img
+          src="${product.images[0]}"
+          class="w-[182px] h-[182px] rounded-[24px]"
+          alt=""
+        />
+        <p class="text-[20px] font-bold w-[180px] overflow-hidden text-ellipsis text-nowrap">${product.title}</p>
+        <p class="text-[16px] font-semibold">$ ${product.price}</p>
+      </div>
+    </a>
+        `;
+      })
+      .join("")}
+    </div>
+  </div>
+  </div>
+  
+      `;
+}
+
 window.search = () => {
   const inp = document.querySelector("#searchInp").value;
   router.navigate(routes.search + `?q=${inp}`);
 };
 
 window.type = () => {
-  let nmd = document.querySelector("#searchInp").value;
-  console.log(nmd);
+  //   let nmd = document.querySelector("#searchInp").value;
+  //   console.log(nmd);
+  console.log("hi");
 };
 
 async function productData(b) {
