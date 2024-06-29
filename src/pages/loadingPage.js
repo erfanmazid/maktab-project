6f570e91087bda75287926428ab0710f15d3ece0
erfan mazidabadi <erfanmazidam@gmail.com>
2024-06-29 12:51:31 +0330

    add shipping type localStorage

--- src/pages/loadingPage.js
+++ src/pages/loadingPage.js
@@ -9,6 +9,7 @@ export default function loadingPage() {
   } else {
     localStorage.searchHistory = JSON.stringify([]);
     localStorage.cartList = JSON.stringify([]);
+    localStorage.shippingType = JSON.stringify({});
     document.querySelector("#app").innerHTML = `
         <div class="flex flex-col items-center h-screen">
         <div class="flex items-center gap-x-2 mt-[360px] mb-[250px]">
