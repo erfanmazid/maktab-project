d12993ab7b4d26d608aeffb7208354681f155e2a
erfan mazidabadi <erfanmazidam@gmail.com>
2024-07-06 21:57:52 +0330

    add nav bar

--- src/pages/products.js
+++ src/pages/products.js
@@ -8,12 +8,15 @@ export default async function productsPage(dataBrand) {
     try {
       renderHTML(dataBrand);
       opt();
+      nav();
     } catch (e) {
       renderHTML(data);
       opt();
+      nav();
     }
     renderHTML(dataBrand);
     opt();
+    nav();
   } else {
     router.navigate(routes.login);
   }
@@ -21,13 +24,13 @@ export default async function productsPage(dataBrand) {
 
 function renderHTML(products) {
   document.getElementById("app").innerHTML = `
-    <div class="flex flex-col gap-y-3 w-full">
+    <div class="flex flex-col gap-y-3 w-full relative h-screen">
     <div class="flex justify-start items-center font-semibold w-full p-5 gap-x-5">
     <i class="fa-solid fa-arrow-left" onclick="back()"></i>
       <p class="text-[20px]">Most Popular</p>
     </div>
     <div
-      class="flex gap-x-3 pl-5 overflow-x-scroll h-[39px] w-full items-center no-scrollbar"
+      class="flex gap-x-3 pl-5 overflow-x-scroll h-[39px] w-full items-center no-scrollbar p-5"
     >
       <div
         class="border-2 py-1 px-4 rounded-[25px] border-[#343A40] text-[#343A40] opt active-option"
@@ -62,7 +65,7 @@ function renderHTML(products) {
       </div>
     </div>
     <div
-      class="flex flex-wrap gap-5 p-5 overflow-y-auto items-center no-scrollbar"
+      class="flex flex-wrap gap-5 p-5 overflow-y-auto items-center no-scrollbar pb-[80px]"
     >
     ${products
       .map((product) => {
@@ -81,6 +84,34 @@ function renderHTML(products) {
         `;
       })
       .join("")}
+    </div>
+        <div
+      class="px-6 pt-6 pb-[10px] w-full absolute bottom-0 bg-white left-0 flex flex-col justify-center items-center rounded-t-[35px] border border-gray-200"
+    >
+      <nav>
+        <ul class="flex gap-x-[44px]">
+          <li class="flex flex-col justify-center items-center gap-y-1 text-gray-400 bar active">
+              <i class="fa-solid fa-house text-[24px]"></i>
+              <p class="text-[10px] font-semibold">Home</p>
+          </li>
+          <li class="flex flex-col justify-center items-center gap-y-1 text-gray-400 bar">
+              <i class="fa-solid fa-bag-shopping text-[24px]"></i>
+              <p class="text-[10px] font-semibold">cart</p>
+          </li>
+          <li class="flex flex-col justify-center items-center gap-y-1 text-gray-400 bar">
+              <i class="fa-solid fa-cart-shopping text-[24px]"></i>
+              <p class="text-[10px] font-semibold">Orders</p>
+          </li>
+          <li class="flex flex-col justify-center items-center gap-y-1 text-gray-400 bar">
+              <i class="fa-solid fa-wallet text-[24px]"></i>
+              <p class="text-[10px] font-semibold">Wallet</p>
+          </li>
+          <li class="flex flex-col justify-center items-center gap-y-1 text-gray-400 bar">
+              <i class="fa-solid fa-user text-[24px]"></i>
+              <p class="text-[10px] font-semibold">Profile</p>
+          </li>
+        </ul>
+      </nav>
     </div>
   </div>
 
@@ -119,4 +150,17 @@ window.opt = () => {
 
 window.back = () => {
   router.navigate(routes.home);
+};
+
+window.nav = () => {
+  const bars = document.querySelectorAll(".bar");
+  bars.forEach((items) => {
+    items.addEventListener("click", () => {
+      document.querySelector(".active").classList.remove("active");
+      items.classList.add("active");
+      const pageName = items.querySelector("p").innerHTML;
+      const newPage = pageName.toLocaleLowerCase();
+      router.navigate(`/${newPage}`);
+    });
+  });
 };
